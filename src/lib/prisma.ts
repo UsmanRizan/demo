2211820207dfced7a3c17d1e5a6d8d8fde787ce4
UsmanRizan <usmanrizan6@gmail.com>
2026-08-16 16:58:33 +0530

    prisma fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,11 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-const adapter = new PrismaPg({
-  connectionString,
-});
-
-export const prisma = new PrismaClient({
-  adapter,
-});
