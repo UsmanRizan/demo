f2b2d795a3c97040c32a8048d942c86d8694338e
UsmanRizan <usmanrizan6@gmail.com>
2026-08-18 10:35:10 +0530

    chore(config): add localhost to allowedDevOrigins
    Update the Next.js configuration to include localhost:3000 in the
    allowedDevOrigins array to ensure consistent development environments
    across different local setups.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ["unnestled-bailey-anxiously.ngrok-free.dev"],
+  allowedDevOrigins: [
+    "unnestled-bailey-anxiously.ngrok-free.dev",
+    "localhost:3000",
+  ],
 };
 
 export default nextConfig;
